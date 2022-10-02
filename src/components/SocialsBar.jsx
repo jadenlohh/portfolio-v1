2890a6d31a111beea62258e3e6789144e80637c7
Jaden loh <65410149+jadenlohh@users.noreply.github.com>
2022-10-02 23:39:51 +0800

    chore: fix minor details

--- src/components/SocialsBar.jsx
+++ src/components/SocialsBar.jsx
@@ -4,7 +4,7 @@ import { Nav } from 'react-bootstrap'
 const SocialsBar = () => {
     return (
         <Nav className="socials-bar flex-column">
-            <Nav.Link className='icons' title='Linkedin'><i className="fa-brands fa-linkedin-in"></i></Nav.Link>
+            <Nav.Link className='icons' href='https://www.linkedin.com/in/jadenlohh/' target='_blank' title='Linkedin'><i className="fa-brands fa-linkedin-in"></i></Nav.Link>
             <Nav.Link className='icons' href='https://www.github.com/jadenlohh' target='_blank' title='Github'><i className="fa-brands fa-github"></i></Nav.Link>
             <Nav.Link className='icons' href='mailto:jadenlohh@gmail.com' title='Email'><i className="fa-regular fa-envelope"></i></Nav.Link>
             <Nav.Link className='icons' href='https://t.me/jadenlohh' target='_blank' title='Telegram'><i className="fa-brands fa-telegram"></i></Nav.Link>
